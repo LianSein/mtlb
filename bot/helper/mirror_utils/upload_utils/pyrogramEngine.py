94423f70726c5489c2811c81f9b39c91562089e2
LianSein <88521488+LianSein@users.noreply.github.com>
2022-07-22 11:05:34 +0530

    Update pyrogramEngine.py

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -62,7 +62,14 @@ class TgUploader:
         self.__listener.onUploadComplete(None, size, self.__msgs_dict, self.__total_files, self.__corrupted, self.name)
 
     def __upload_file(self, up_path, file_, dirpath):
-
+        # print full path file location +
+        keption = file_
+        if not keption.endswith('/'): keption = keption + '/'
+        if not keption.startswith('/'): keption = '/' + keption
+        keption = file_.replace(keption, '', 1)
+        zoy = keption.split('/')[0]
+        keption = keption.replace(zoy, '', 1)
+        if keption.startswith('/'): keption = keption.replace('/', '', 1)
         # print full path file location -
         if CUSTOM_FILENAME is not None:
             cap_mono = f"{CUSTOM_FILENAME} <code>{keption}</code>" #f"{CUSTOM_FILENAME} <code>{file_}</code>"
